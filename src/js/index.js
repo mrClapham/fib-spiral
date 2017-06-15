685c3386dce4f0f19cffce71232243c2b40d1d21
Graham Clapham <grahamclapham@Grahams-new-MacBook-Pro.local>
2017-06-15 22:38:26 +0100

    Added D# chart - currently not working correctly.

--- src/js/index.js
+++ src/js/index.js
@@ -3,6 +3,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from '../js/components/App/App.js';
 import FibSpiral from '../js/vanilla_conmponents/fib-spiral';
+import mockData from '../js/vanilla_conmponents/mocks/PieData2LayerMock'
+import PieChart2layer from '../js/vanilla_conmponents/pieChart2layer'
 require("../sass/entry.scss");
 
 document.addEventListener("DOMContentLoaded", function () {
@@ -10,6 +12,12 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 function init() {
+
+            const _data = new  mockData();
+            const _d1 =    _data.getRandom()
+            const _pie = PieChart2layer.create('#piechart', _d1, {})
+
+
 // Pure JS renderer.
     const _header = FibSpiral.create({ text: "Default headline text" })
     .render(document.querySelector('#content-vanilla'));
